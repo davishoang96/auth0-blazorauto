08bef84119459ea3f4235e5390543d8f8a416f82
Viet Hoang <vhoang@zoodata.com.au>
2024-08-16 21:48:04 +0800

    init

---
+++ BlazorIntAuto/Controllers/InternalDataController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorIntAuto.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("[Controller]")]
+    public class InternalDataController : ControllerBase
+    {
+        [HttpGet("GetData")]
+        public IEnumerable<int> GetData()
+        {
+            return Enumerable.Range(1, 5).Select(index =>
+                Random.Shared.Next(1, 100))
+                .ToArray();
+        }
+    }
+}
---
+++ BlazorIntAuto/Program.cs
@@ -0,0 +1,90 @@
+using BlazorIntAuto.Client.Pages;
+using BlazorIntAuto.Components;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddControllers();
+builder.Services
+    .AddAuth0WebAppAuthentication(options => {
+      options.Domain = builder.Configuration["Auth0:Domain"];
+      options.ClientId = builder.Configuration["Auth0:ClientId"];
+      options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+    })
+    .WithAccessToken(options =>
+      {
+          options.Audience = builder.Configuration["Auth0:Audience"];
+      });
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<TokenHandler>();
+
+builder.Services.AddHttpClient("ExternalAPI", 
+      client => client.BaseAddress = new Uri(builder.Configuration["ExternalApiBaseUrl"]))
+      .AddHttpMessageHandler<TokenHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+  .CreateClient("ExternalAPI"));
+
+var app = builder.Build();
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapGet("/Account/Login", async (HttpContext httpContext, string redirectUri = "/") =>
+{
+  var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+          .WithRedirectUri(redirectUri)
+          .Build();
+
+  await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+
+app.MapGet("/Account/Logout", async (HttpContext httpContext, string redirectUri = "/") =>
+{
+  var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+          .WithRedirectUri(redirectUri)
+          .Build();
+
+  await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+  await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
+
+app.MapGet("/api/externalData", async (HttpClient httpClient) =>
+{
+    return await httpClient.GetFromJsonAsync<int[]>("data");
+})
+.RequireAuthorization();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
